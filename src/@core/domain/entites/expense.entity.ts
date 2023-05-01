ae6f5760658424fb23fe57cf91dc784bc4bc26f0
Felipe Santos <rfelipe273@gmail.com>
2023-05-01 15:25:32 -0300

    feat: add expense class props

--- src/@core/domain/entites/expense.entity.ts
+++ src/@core/domain/entites/expense.entity.ts
@@ -9,7 +9,22 @@ type Input = {
   location: string;
 };
 export class Expense {
+  id: string;
+  description: string;
+  value: number;
+  date: Date;
+  categoryId: string;
+  paymentMethod: string;
+  bankId: string;
+  location: string;
   constructor(input: Input) {
-    //  TODO: validate input
+    this.id = input.id;
+    this.description = input.description;
+    this.value = input.value;
+    this.date = input.date;
+    this.categoryId = input.categoryId;
+    this.paymentMethod = input.paymentMethod;
+    this.bankId = input.bankId;
+    this.location = input.location;
   }
 }
--- src/@core/domain/entites/index.ts
+++ src/@core/domain/entites/index.ts
@@ -1,5 +1,6 @@
 import { Expense } from "./expense.entity";
 import { Bank } from "./bank.entity";
 import { Income } from "./income.entity";
+import { Category } from "./category.entity";
 
-export { Expense, Bank, Income };
+export { Expense, Bank, Income, Category };
